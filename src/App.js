6afadb1bccfaa3a4845a55df6753789caf1f76df
markwasmuth <markwasmuth@gmail.com>
2025-10-07 11:29:13 -0400

    remove script

--- src/App.js
+++ src/App.js
@@ -186,8 +186,7 @@ const SnapToSellLanding = () => {
   <p className="text-sm text-gray-500 mt-4">2,847 people already signed up</p>
 </div>
                   {`var d=document,w="https://tally.so/widgets/embed.js",v=function(){"undefined"!=typeof Tally?Tally.loadEmbeds():d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach((function(e){e.src=e.dataset.tallySrc}))};if("undefined"!=typeof Tally)v();else if(d.querySelector('script[src="'+w+'"]')==null){var s=d.createElement("script");s.src=w,s.onload=v,s.onerror=v,d.body.appendChild(s);}`}
-                </script>
-              </div>
+                       </div>
             </div>
 
             {/* Stats */}
